60808c5b5c9b7ca7ac259bad9b73e0304965e9e6
Carl Sundvisson <carl@jeesoft.uk>
2021-04-23 14:06:47 +0200

    Typescript added, first tests are running

---
+++ rebeccas-test-reporter/src/index.ts
@@ -0,0 +1,3 @@
+import { Able } from "./Able";
+export default Able;
+Object.assign(module.exports, Able);
---
+++ rebeccas-test-reporter/test/index.spec.ts
@@ -0,0 +1,6 @@
+// import Able from "../src";
+import { Able } from "../src/Able";
+
+test("Able is exported", () => {
+  expect(Able).toBeInstanceOf(Object);
+});
