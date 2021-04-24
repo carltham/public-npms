f332c4d7014179d8d2e73c688334cf8e3220c6fb
Carl Sundvisson <carl@jeesoft.uk>
2021-04-24 23:08:22 +0200

    All tests are running

--- rebeccas-test-reporter/test-build.js
+++ rebeccas-test-reporter/test-build.js
@@ -1,9 +1,10 @@
 const assert = require("assert");
-const Able = require("./dist");
+const Able = require("./dist/Able");
 
 const definition = { foo: ["bar"] };
 const abilities = ["foo", "bam"];
-const result = Able.flatten(definition, abilities).sort();
+console.log("Able = ", Able);
+const result = Able.Able.flatten(definition, abilities).sort();
 assert.deepStrictEqual(result, ["foo", "bar", "bam"].sort());
 
 console.log("OK!");
