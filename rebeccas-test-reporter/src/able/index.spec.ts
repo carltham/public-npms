fdf82aca28836872078fbe740052d0e44f3e0cad
Carl Sundvisson <carl@jeesoft.uk>
2021-04-25 01:09:30 +0200

    First file translated index.js->.ts

--- rebeccas-test-reporter/src/able/index.spec.ts
+++ rebeccas-test-reporter/src/able/index.spec.ts
@@ -1,5 +1,6 @@
 // import Able from "../src";
-import { Able } from "../src/Able";
+
+import { Able } from "./able";
 
 test("Able is exported", () => {
   expect(Able).toBeInstanceOf(Object);
--- rebeccas-test-reporter/test-build.js
+++ rebeccas-test-reporter/test-build.js
@@ -1,10 +1,15 @@
 const assert = require("assert");
-const Able = require("./dist/Able");
+const Able = require("./dist/able");
+const Startup = require("./dist/rebecca/index");
 
 const definition = { foo: ["bar"] };
 const abilities = ["foo", "bam"];
 console.log("Able = ", Able);
-const result = Able.Able.flatten(definition, abilities).sort();
+const result = Able.flatten(definition, abilities).sort();
 assert.deepStrictEqual(result, ["foo", "bar", "bam"].sort());
+
+console.log("OK!");
+
+console.log("Startup = ", Startup);
 
 console.log("OK!");
